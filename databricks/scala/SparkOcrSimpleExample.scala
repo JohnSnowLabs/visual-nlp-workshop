4e71a86609c24aa92212011d673ecf49297041be
Mykola Melnyk <kolia1985@gmail.com>
2020-05-11 09:08:51 +0300

    Revert "Rnamed OCR to ImageToText"

--- databricks/scala/SparkOcrSimpleExample.scala
+++ databricks/scala/SparkOcrSimpleExample.scala
@@ -63,7 +63,7 @@ def pipeline() = {
       .setOutputCol("image")
 
     // Run tesseract OCR
-    val ocr = new ImageToText()
+    val ocr = new TesseractOcr()
       .setInputCol("image")
       .setOutputCol("text")
       .setConfidenceThreshold(65)
