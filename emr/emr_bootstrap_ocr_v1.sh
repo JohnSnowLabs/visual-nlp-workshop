e85e2dadf9e523cfba0a0a438af26215d5dccfcf
xyutech <klg-testing@yandex.ru>
2022-03-12 17:26:10 +0300

    Initial bootstrap script

---
+++ emr/emr_bootstrap_ocr_v1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x -e
+
+echo -e 'export PYSPARK_PYTHON=/usr/bin/python3 
+export HADOOP_CONF_DIR=/etc/hadoop/conf 
+export SPARK_JARS_DIR=/usr/lib/spark/jars 
+export SPARK_HOME=/usr/lib/spark' >> $HOME/.bashrc && source $HOME/.bashrc
+
+echo "$1"
+version=`echo "$1" | cut -d\- -f1`
+echo "$version"
+
+sudo python3 -m pip install numpy==1.21.5
+sudo python3 -m pip install pillow==9.0.1
+sudo python3 -m pip install imageio==2.16.0
+sudo python3 -m pip install --upgrade spark-ocr==$version+spark30 --extra-index-url=https://pypi.johnsnowlabs.com/$1
+
+sudo wget https://pypi.johnsnowlabs.com/$1/jars/spark-ocr-assembly-$version-spark30.jar -P /usr/lib/spark/jars
+
+set +x
+exit 0
