d257a8427b185946d1eea33e461bd1f8069dca09
xyutech <klg-testing@yandex.ru>
2022-03-12 21:02:19 +0300

    Adopted script to amazon

--- emr/emr_bootstrap_ocr_v1.sh
+++ emr/emr_bootstrap_ocr_v1.sh
@@ -10,12 +10,11 @@ echo "$1"
 version=`echo "$1" | cut -d\- -f1`
 echo "$version"
 
-sudo python3 -m pip install numpy==1.21.5
-sudo python3 -m pip install pillow==9.0.1
-sudo python3 -m pip install imageio==2.16.0
+sudo python3 -m pip install numpy==1.21.5 pillow==9.0.1 imageio==2.16.0
 sudo python3 -m pip install --upgrade spark-ocr==$version+spark30 --extra-index-url=https://pypi.johnsnowlabs.com/$1
 
 sudo wget https://pypi.johnsnowlabs.com/$1/jars/spark-ocr-assembly-$version-spark30.jar -P /usr/lib/spark/jars
+sudo mv /usr/lib/spark/jars/spark-ocr-assembly-$version-spark30.jar /usr/lib/spark/jars/spark-ocr-assembly-$version-spark301+amzn.jar
 
 set +x
 exit 0
