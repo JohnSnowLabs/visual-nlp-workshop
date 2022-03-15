680a26c9f3ebd531ba6a06e411b291373368b3df
xyutech <klg-testing@yandex.ru>
2022-03-15 14:01:57 +0300

    Fixed issues. Working set of files

--- emr/emr_bootstrap_ocr_v1.sh
+++ emr/emr_bootstrap_ocr_v1.sh
@@ -11,11 +11,12 @@ version=`echo "$1" | cut -d\- -f1`
 echo "$version"
 
 sudo python3 -m pip install --upgrade pip setuptools
-sudo python3 -m pip install numpy==1.21.5 pillow==9.0.1 imageio==2.16.0 pip ipython
+sudo python3 -m pip install numpy==1.21.5 pillow==9.0.1 imageio==2.16.0 pip ipython spark-nlp==3.4.0
 sudo python3 -m pip install --upgrade spark-ocr==$version+spark30 --extra-index-url=https://pypi.johnsnowlabs.com/$1
 
 sudo wget https://pypi.johnsnowlabs.com/$1/jars/spark-ocr-assembly-$version-spark30.jar -P /usr/lib/spark/jars
-sudo mv /usr/lib/spark/jars/spark-ocr-assembly-$version-spark30.jar /usr/lib/spark/jars/spark-ocr-assembly-$version-spark301+amzn.jar
+#sudo mv /usr/lib/spark/jars/spark-ocr-assembly-$version-spark30.jar /usr/lib/spark/jars/spark-ocr-assembly-$version-spark301+amzn.jar
+sudo wget https://s3.amazonaws.com/auxdata.johnsnowlabs.com/public/jars/spark-nlp-assembly-3.4.0.jar -P /usr/lib/spark/jars
 
 set +x
 exit 0
