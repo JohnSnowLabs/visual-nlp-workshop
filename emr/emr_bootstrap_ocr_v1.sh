55cc70f679ffcfdf66eb96ee6282f2c4723e3f3b
xyutech <klg-testing@yandex.ru>
2022-03-20 14:53:48 +0300

    Changed jar path beack to main source

--- emr/emr_bootstrap_ocr_v1.sh
+++ emr/emr_bootstrap_ocr_v1.sh
@@ -14,8 +14,9 @@ sudo python3 -m pip install --upgrade pip setuptools
 sudo python3 -m pip install numpy==1.21.5 pillow==9.0.1 imageio==2.16.0 pip ipython spark-nlp==3.4.0
 sudo python3 -m pip install --upgrade spark-ocr==$version+spark30 --extra-index-url=https://pypi.johnsnowlabs.com/$1
 
+#sudo wget https://www.dropbox.com/s/f0o8flw8v2we3h3/spark-ocr-assembly-3.11.0-spark30.jar -P /usr/lib/spark/jars
 sudo wget https://pypi.johnsnowlabs.com/$1/jars/spark-ocr-assembly-$version-spark30.jar -P /usr/lib/spark/jars
-#sudo mv /usr/lib/spark/jars/spark-ocr-assembly-$version-spark30.jar /usr/lib/spark/jars/spark-ocr-assembly-$version-spark301+amzn.jar
+sudo mv /usr/lib/spark/jars/spark-ocr-assembly-$version-spark30.jar /usr/lib/spark/jars/spark-ocr-assembly-$version-spark301+amzn.jar
 sudo wget https://s3.amazonaws.com/auxdata.johnsnowlabs.com/public/jars/spark-nlp-assembly-3.4.0.jar -P /usr/lib/spark/jars
 
 set +x
