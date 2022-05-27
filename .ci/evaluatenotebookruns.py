b81c85384a23cffc4017d576989526f83463964a
Mykola Melnyk <kolia1985@gmail.com>
2022-05-27 09:36:30 +0300

    Updated Jenkinsfile

--- .ci/evaluatenotebookruns.py
+++ .ci/evaluatenotebookruns.py
@@ -35,10 +35,14 @@ class TestJobOutput(unittest.TestCase):
         for filename in glob.glob(os.path.join(path, '*.json')):
             logging.info('Evaluating: ' + filename)
             data = json.load(open(filename))
-            status = data['state']['result_state']
-            statuses.append(status)
+            if data['state']['life_cycle_state'] == "RUNNING":
+                statuses.append('NOT_COMPLETED')
+            else:
+                status = data['state']['result_state']
+                statuses.append(status)
 
         self.assertFalse('FAILED' in statuses)
+        self.assertFalse('NOT_COMPLETED' in statuses)
 
 if __name__ == '__main__':
     unittest.main()
